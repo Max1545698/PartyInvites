e4a0c8d6d137904aab3e68937d056176ceb106f4
Максим Рожок <max15071999@gmail.com>
2018-07-18 18:03:32 +0300

    Admin v2

---
+++ SportsStore/Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public class AdminController : Controller
+    {
+        IProductRepository repository;
+
+        public AdminController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+        public ViewResult Index()
+        {
+            return View(repository.Products);
+        }
+    }
+}
