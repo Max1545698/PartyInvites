25a69e9176a382413fee0a374bc175c8fbb320ce
Максим Рожок <max15071999@gmail.com>
2018-07-18 16:15:48 +0300

    Administrating v1

--- SportsStore/Controllers/OrderController.cs
+++ SportsStore/Controllers/OrderController.cs
@@ -17,6 +17,24 @@ namespace SportsStore.Controllers
             cart = cartService;
         }
 
+        public ViewResult List() =>
+            View(repository.Orders.Where(o => !o.Shipped));
+
+        [HttpPost]
+        public IActionResult MarkShipped(int orderId)
+        {
+            Order order = repository.Orders
+                .FirstOrDefault(o => o.OrderID == orderId);
+
+            if(order != null)
+            {
+                order.Shipped = true;
+                repository.SaveOrder(order);
+            }
+
+            return RedirectToAction(nameof(List));
+        }
+
         public ViewResult Checkout()
         {
             return View(new Order());
