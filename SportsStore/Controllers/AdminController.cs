980c46aa0af81d8ab17c7f4c1ff709154f9467d2
Максим Рожок <max15071999@gmail.com>
2018-07-20 19:00:00 +0300

    Front-end upgrade

--- SportsStore/Controllers/AdminController.cs
+++ SportsStore/Controllers/AdminController.cs
@@ -37,5 +37,22 @@ namespace SportsStore.Controllers
                 return View(product);
             }
         }
+
+        public ViewResult Create()
+        {
+            return View("Edit", new Product());
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int productID)
+        {
+            Product deletedProduct = repository.DeleteProduct(productID);
+            if(deletedProduct != null)
+            {
+                TempData["message"] = $"{deletedProduct.Name} was deleted";
+            }
+
+            return RedirectToAction("Index");
+        }
     }
 }
